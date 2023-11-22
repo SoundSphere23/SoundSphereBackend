9ca2152f78851556de87f5d8f9af314f802faa75
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2023-11-22 15:09:51 +0100

    feature: jwt middleware

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,26 +1,26 @@
 import { Response, Request } from "express";
-import { prismaClient } from '../db/client'
+import { prismaClient } from "../db/client";
 
 // ejemplos de función => controller
 export const getAllUsers = async (req: Request, res: Response) => {
-    try {
-        const users = await prismaClient.user.findMany({});
-        res.status(201).json(users);
-    } catch (error) {
-        res.status(500).json(error);
-    }
+  try {
+    console.log("init get all users");
+    const users = await prismaClient.user.findMany({});
+    res.status(201).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 export const createUser = async (req: Request, res: Response) => {
-    const { name, email} = req.body;
+  const { name, email } = req.body;
 
-    try {
-        
-        const newUser = await prismaClient.user.create({ data: { name, email } });
+  try {
+    const newUser = await prismaClient.user.create({ data: { name, email } });
 
-        res.status(201).json(newUser);
-    } catch (error) {
-        console.error("Error creating user:", error);
-        res.status(500).json(error);
-    }
-}
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json(error);
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import express, {Request, Response} from 'express';
-import userRoutes from './routes/user.routes';
+import express, { Request, Response } from "express";
+import userRoutes from "./routes/user.routes";
+import { jwtCheckMiddleware } from "./middlewares/jwtCheck";
 
-const cors = require("cors")
-const app = express()
-app.use(express.json())
-app.use(cors())
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use("/user", userRoutes)
+app.use("/user", jwtCheckMiddleware, userRoutes);
 
-
-
-export default app
+export default app;
